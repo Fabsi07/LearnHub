fbdd6457225ff26a8a36b83197cf4fd631446b00
Fabsi07 <fabi.winterhalter@gmail.com>
2026-04-27 18:44:58 +0200

    feat: update app title from 'Learnmanagement App' to 'StudyHub'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>Learnmanagement App</h1>
+      <h1>StudyHub</h1>
       <p>Projekt-Skeleton fuer die weitere Entwicklung.</p>
     </main>
   )
